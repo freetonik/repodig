30533c27b534d2d3486ce5f4f784617cd162cc07
Sam <sam@samekkert.com>
2019-12-15 17:35:50 +0200

    change redis url

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_client do |config|
-  config.redis = { url: "redis://#{ENV['DATA_REDIS_HOST']}:6379/5" }
+  config.redis = { url: "redis://#{ENV['REDIS_URL']}:6379/5" }
 end
 
 Sidekiq.configure_server do |config|
-    config.redis = { url: "redis://#{ENV['DATA_REDIS_HOST']}:6379/5" }
+    config.redis = { url: "redis://#{ENV['REDIS_URL']}:6379/5" }
 end
